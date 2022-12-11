1231cc3a4ea21a58ad02ea4077220a4144c27876
Alexandr Belyaev <eminis.ru@gmail.com>
2022-12-11 21:55:06 +0300

    Add four methods in Teacher

--- SeparatorIntoGroup/Teacher.cs
+++ SeparatorIntoGroup/Teacher.cs
@@ -4,6 +4,8 @@ namespace SeparatorIntoGroup;
 
 public class Teacher : AbstractPersons
 {
+    private ProjectCore _storage;
+
     public Teacher(int id, string name, string userName)
     {
         Id = id;
@@ -11,4 +13,28 @@ public class Teacher : AbstractPersons
         AccountName = userName;
         Status = StatusType.IsTeacher;
     }
+
+    public void AddStudent(Student student)
+        {
+            _storage.Students.Add(student);
+            _storage.SaveAll();
+        }
+
+        public void DeleteStudent(Student student)
+        {
+            _storage.Students.Remove(student);
+            _storage.SaveAll();
+        }
+
+        public void AddGroup (Group group)
+        {
+            _storage.Groups.Add(group);
+            _storage.SaveAll();
+        }
+
+        public void DeleteGroup(Group group)
+        {
+            _storage.Groups.Remove(group);
+            _storage.SaveAll();
+        }
 }
