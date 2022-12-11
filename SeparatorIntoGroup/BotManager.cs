a46752af32459af4eb9f0c4caa9bbac05b17a687
Vlad <VIStrelnikov94@gmail.com>
2022-12-12 01:01:38 +0300

    add BotManager Ask method + ProjectCore Load methodd

--- SeparatorIntoGroup/BotManager.cs
+++ SeparatorIntoGroup/BotManager.cs
@@ -2,4 +2,12 @@
 
 public class BotManager
 {
+    public void Ask(Student student)
+    {
+        Questionnaire question = new Questionnaire();
+        question.QuestionAboutWishStudents(new List<Student>());
+        question.QuestionAboutNotWishStudents(new List<Student>());
+        question.QuestionAboutTime("18:50", "2");
+        student.AnswersToQuestionnaire = question;   
+    }
 }
--- SeparatorIntoGroup/ProjectCore.cs
+++ SeparatorIntoGroup/ProjectCore.cs
@@ -4,29 +4,45 @@ namespace SeparatorIntoGroup;
 
 public class ProjectCore
 {
-        public List<Teacher> Teachers { get; set; }
-        public List<Student> Students { get; set; }
-        public List<Group> Groups { get; set; }
-        public string Path { get; set; }
+    public List<Teacher> Teachers { get; set; }
+    public List<Student> Students { get; set; }
+    public List<Group> Groups { get; set; }
+    public string Path { get; set; }
 
-        public ProjectCore() 
-        { 
-            Teachers = new List<Teacher>();
-            Students = new List<Student>();
-            Groups = new List<Group>();
-            Path = "../Storage.txt";
-        }
+    public ProjectCore()
+    {
+        Teachers = new List<Teacher>();
+        Students = new List<Student>();
+        Groups = new List<Group>();
+        Path = "../Storage.txt";
+    }
 
-        public void SaveAll()
+    public void SaveAll()
+    {
+        using (StreamWriter sw = new StreamWriter(Path))
         {
-            using (StreamWriter sw = new StreamWriter(Path))
+            string jsn = JsonSerializer.Serialize(Teachers);
+            sw.WriteLine(jsn);
+            jsn = JsonSerializer.Serialize(Students);
+            sw.WriteLine(jsn);
+            jsn = JsonSerializer.Serialize(Groups);
+            sw.WriteLine(jsn);
+        }
+    }
+
+    public void LoadAll()
+    {
+        if (File.Exists(Path))
+        {
+            using (StreamReader sr = new StreamReader(Path))
             {
-                string jsn = JsonSerializer.Serialize(Teachers);
-                sw.WriteLine(jsn);
-                jsn = JsonSerializer.Serialize(Students);
-                sw.WriteLine(jsn);
-                jsn = JsonSerializer.Serialize(Groups);
-                sw.WriteLine(jsn);
+                string jsn = sr.ReadLine();
+                Teachers = JsonSerializer.Deserialize<List<Teacher>>(jsn);
+                jsn = sr.ReadLine();
+                Students = JsonSerializer.Deserialize<List<Student>>(jsn);
+                jsn = sr.ReadLine();
+                Groups = JsonSerializer.Deserialize<List<Group>>(jsn);
             }
         }
+    }
 }
